6ece3dc65518bc35ecd18e4a116e9a274e45085e
etodorov_tum <evgeni.todorov@tum.de>
2021-11-04 20:00:09 +0100

    bash file to test options on the login node

---
+++ assignment1/heat_base/test_opts_login.sh
@@ -0,0 +1,15 @@
+flagarray=("-O0" "-O1" "-O2" "-O3" "-Ofast" "-xhost" "-ipo" "-fno-alias" "-xCORE-AVX512")
+
+echo "Run on login node: "`date` >> full.out
+echo "Run on login node: "`date` >> filtered.out
+
+
+for i in "${flagarray[@]}"
+do
+    make clean
+    sed 's/^CFLAGS.*/CFLAGS = -O1/' -i Makefile
+    make
+    ./heat test.dat >> full.out
+    echo "#CFLAGS = "$i >> filtered.out
+    tail -6 full.out >> filtered.out
+done
