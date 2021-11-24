e238acc52d0fb66439eb914e015d0a616899bbd2
Evgeni Todorov <evgeni.todorov@tum.de>
2021-11-24 22:15:23 +0100

    block jacobi 256

--- assignment3/heat_base/relax_jacobi.c
+++ assignment3/heat_base/relax_jacobi.c
@@ -7,6 +7,15 @@
 
 #include "heat.h"
 
+#define BLOCK_SIZE 256
+
+#ifndef max
+#define max(a,b)            (((a) > (b)) ? (a) : (b))
+#endif
+
+#ifndef min
+#define min(a,b)            (((a) < (b)) ? (a) : (b))
+#endif
 /*
  * Residual (length of error vector)
  * between current solution and next after a Jacobi step
@@ -15,17 +24,22 @@ double residual_jacobi(double *u, unsigned sizex, unsigned sizey) {
 	unsigned i, j;
 	double unew, diff, sum = 0.0;
 
-	for (i = 1; i < sizey - 1; i++){
-		for (j = 1; j < sizex - 1; j++) {
-			unew = 0.25 * (u[i * sizex + (j - 1)] +  // left
-						   u[i * sizex + (j + 1)] +  // right
-						   u[(i - 1) * sizex + j] +  // top
-						   u[(i + 1) * sizex + j]); // bottom
+	for (int by=0; by < sizey/BLOCK_SIZE; by++){
+		for (int bx=0; bx < sizex/BLOCK_SIZE; bx++){
+			for (i = max(1, by*BLOCK_SIZE); i < min((by+1)*BLOCK_SIZE, sizey - 1); i++){
+				for (j = max(1, bx*BLOCK_SIZE); j < min((bx+1)*BLOCK_SIZE, sizex - 1); j++) {
+					unew = 0.25 * (u[i * sizex + (j - 1)] +  // left
+								u[i * sizex + (j + 1)] +  // right
+								u[(i - 1) * sizex + j] +  // top
+								u[(i + 1) * sizex + j]); // bottom
 
-			diff = unew - u[i * sizex + j];
-			sum += diff * diff;
+					diff = unew - u[i * sizex + j];
+					sum += diff * diff;
+				}
+			}
 		}
 	}
+
 
 	return sum;
 }
@@ -36,13 +50,16 @@ double residual_jacobi(double *u, unsigned sizex, unsigned sizey) {
 void relax_jacobi(double* restrict u, double * restrict utmp, unsigned sizex, unsigned sizey) {
 	int i, j;
 
-	for (i = 1; i < sizey - 1; i++) {
-		for (j = 1; j < sizex - 1; j++) {
-			utmp[i * sizex + j] = 0.25 * (u[i * sizex + (j - 1)] +  // left
-										  u[i * sizex + (j + 1)] +  // right
-										  u[(i - 1) * sizex + j] +  // top
-										  u[(i + 1) * sizex + j]); // bottom
+	for (int by=0; by < sizey/BLOCK_SIZE; by++){
+		for (int bx=0; bx < sizex/BLOCK_SIZE; bx++){
+			for (i = max(1, by*BLOCK_SIZE); i < min((by+1)*BLOCK_SIZE, sizey - 1); i++){
+				for (j = max(1, bx*BLOCK_SIZE); j < min((bx+1)*BLOCK_SIZE, sizex - 1); j++) {
+					utmp[i * sizex + j] = 0.25 * (u[i * sizex + (j - 1)] +  // left
+												u[i * sizex + (j + 1)] +  // right
+												u[(i - 1) * sizex + j] +  // top
+												u[(i + 1) * sizex + j]); // bottom
+				}
+			}
 		}
 	}
-	
 }
