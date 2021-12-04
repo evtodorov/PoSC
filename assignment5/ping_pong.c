ae68849bf5c8ef2e783531a5f17c701096e45802
Rahul Banerjee <rbanerjee28@gmail.com>
2021-12-04 06:10:26 +0100

    some changes to bandwidth calculation

--- assignment5/ping_pong.c
+++ assignment5/ping_pong.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <mpi.h>
 
-#define ROUNDS 1000
+#define ROUNDS 10000
 
 int main(int argc, char *argv[])
 {
@@ -29,19 +29,21 @@ int main(int argc, char *argv[])
         for(int round=0; round<ROUNDS; round++)
         {   
             
-            MPI_Send(&data[0], length, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
-            MPI_Recv(&data[0], length, MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(data, length, MPI_CHAR, 1, 10, MPI_COMM_WORLD);
+            MPI_Recv(data, length, MPI_CHAR, 1, 20, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
         end_time = MPI_Wtime();
+
         duration = (end_time - start_time) / ROUNDS;
-        printf("\nData size: 2^%d, Time(ms) : %lf \n", length, duration * 1000);            
+        double bandwidth = pow(2, length) / duration;
+        printf("\nData size : 2^%d (B), Time (ms) : %15.9f, Bandwidth (GB/s) : %15.9f\n", length, duration * 1000, bandwidth / 1000000);
     }
     else if(rank==1)
     {
         for(int round=0; round<ROUNDS; round++)
         {
-            MPI_Recv(&data[0], length, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Send(&data[0], length, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+            MPI_Recv(data, length, MPI_CHAR, 0, 10, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(data, length, MPI_CHAR, 0, 20, MPI_COMM_WORLD);
         }
 
     }
