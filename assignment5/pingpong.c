f1486995b25b9968f0307ef3b3dd8e0cee0742cc
Rahul Banerjee <rbanerjee28@gmail.com>
2021-12-11 06:55:39 +0100

    changed malloc() to calloc()

--- assignment5/pingpong.c
+++ assignment5/pingpong.c
@@ -3,14 +3,14 @@
 #include <math.h>
 #include <mpi.h>
 
-#define ROUNDS 10000
+#define ROUNDS 100 // Number of ping-pong exchanges for statistical averaging
 
 int main(int argc, char *argv[])
 {
     int rank, size, init;
     double start_time, end_time, duration;
     int length = atoi(argv[1]);
-    char *data = (char *)malloc(pow(2,length) * sizeof(char));
+    char *data = (char *)calloc(pow(2,length), sizeof(char)); // data chunk of size 2^length Bytes
     init = MPI_Init(NULL,NULL);
     
     if(init != MPI_SUCCESS)
@@ -29,19 +29,18 @@ int main(int argc, char *argv[])
 
     if(rank==0)
     {
-        printf("\nStarting Measurement with data-size 2^%d", length);
+        printf("\nStarting Measurement with data-size 2^%d Bytes", length);
         start_time = MPI_Wtime(); 
         for(int round=0; round<ROUNDS; round++)
         {   
-            
             MPI_Send(data, length, MPI_CHAR, 1, 10, MPI_COMM_WORLD);
             MPI_Recv(data, length, MPI_CHAR, 1, 20, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
         end_time = MPI_Wtime();
 
-        duration = (end_time - start_time) / ROUNDS;
-        double bandwidth = pow(2, length) / duration;
-        printf("\nData size : 2^%d (B), Time (ms) : %15.9f, Bandwidth (MB/s) : %15.9f\n", length, duration * 1000, bandwidth / 1.0e+6);
+        duration = (end_time - start_time) / (2 * ROUNDS);
+        long double bandwidth = pow(2, length) / duration;
+        printf("\nData size : 2^%d (B), Time (s) : %15.9f, Bandwidth (B/s) : %15.9f\n", length, duration, bandwidth);
     }
     else if(rank==1)
     {
