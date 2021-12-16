26a83be3b52b7215086893bee50d5e596a81645d
Hemraj Chauhan <hemraj.chauhan@tum.de>
2021-12-16 17:22:40 +0100

    added Task 1

--- assignment6/HeatForOMP/heat.c
+++ assignment6/HeatForOMP/heat.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <mpi.h>
 #include "input.h"
 #include "heat.h"
 #include "timing.h"
@@ -8,7 +8,7 @@
 double* time;
 
 void usage(char *s) {
-	fprintf(stderr, "Usage: %s <input file> [result file]\n\n", s);
+	fprintf(stderr, "Usage: %s <input file> <X-dimensions> <Y-dimensions> [result file]\n\n", s);
 }
 
 int main(int argc, char *argv[]) {
@@ -16,7 +16,8 @@ int main(int argc, char *argv[]) {
 	FILE *infile, *resfile;
 	char *resfilename;
 	int np, iter, chkflag;
-
+	int dims[2], periods[2], coord[2], reorder;
+	int up, down, left, right;
 	
 	// algorithmic parameters
 	algoparam_t param;
@@ -28,8 +29,15 @@ int main(int argc, char *argv[]) {
 	// set the visualization resolution
 	param.visres = 100;
 
+	int rank, size;
+	MPI_Status s;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm comm_2d;
+
 	// check arguments
-	if (argc < 2) {
+	if (argc < 4) {
 		usage(argv[0]);
 		return 1;
 	}
@@ -42,8 +50,24 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
+	// Dimensions for virtual topology
+	dims[0] = atoi(argv[2]);
+	dims[1] = atoi(argv[3]);
+	periods[0] = 1;
+	periods[1] = 1;
+	reorder = 0;
+
+    if(size != dims[0]*dims[1])
+    {
+        printf("The product of dimensions %d is not equal to number of requested processes %d\n", dims[0]*dims[1], size);
+        MPI_Abort(MPI_COMM_WORLD,1);
+    }
+
+	if (rank == 0)
+		printf(" %d processes distributed in X-direction\n %d processes distributed in Y-direction\n", dims[0], dims[1]);
+
 	// check result file
-	resfilename = (argc >= 3) ? argv[2] : "heat.ppm";
+	resfilename = (argc >= 5) ? argv[4] : "heat.ppm";
 
 	if (!(resfile = fopen(resfilename, "w"))) {
 		fprintf(stderr, "\nError: Cannot open \"%s\" for writing.\n\n", resfilename);
@@ -60,12 +84,25 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	print_params(&param);
+	if(rank == 0)
+		print_params(&param);
+	
 	time = (double *) calloc(sizeof(double), (int) (param.max_res - param.initial_res + param.res_step_size) / param.res_step_size);
 
 	int exp_number = 0;
 
+	// Create Virtual topology
+	MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &comm_2d);
+	MPI_Cart_coords(comm_2d, rank, 2, coord);
+	MPI_Cart_shift(comm_2d, 0, 1, &up, &down);
+	MPI_Cart_shift(comm_2d, 1, 1, &left, &right);
+	printf("rank= %d coords= %d %d neighbours(up,down,left,right)= %d %d %d %d\n", rank, coord[0], coord[1], up, down, left, right);
+
 	for (param.act_res = param.initial_res; param.act_res <= param.max_res; param.act_res = param.act_res + param.res_step_size) {
+		
+		np = param.act_res + 2;
+		int send_count = np*np;
+
 		if (!initialize(&param)) {
 			fprintf(stderr, "Error in Jacobi initialization.\n\n");
 
@@ -77,17 +114,17 @@ int main(int argc, char *argv[]) {
 				param.uhelp[i * (param.act_res + 2) + j] = param.u[i * (param.act_res + 2) + j];
 			}
 		}
-
+		
 		// starting time
 		time[exp_number] = wtime();
 		residual = 999999999;
-		np = param.act_res + 2;
 
 		for (iter = 0; iter < param.maxiter; iter++) {
 			residual = relax_jacobi(&(param.u), &(param.uhelp), np, np);
 		}
-
+		
 		time[exp_number] = wtime() - time[exp_number];
+		
 
 		printf("\n\nResolution: %u\n", param.act_res);
 		printf("===================\n");
@@ -107,5 +144,10 @@ int main(int argc, char *argv[]) {
 	write_image(resfile, param.uvis, param.visres + 2, param.visres + 2);
 
 	finalize(&param);
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	MPI_Finalize();
+	
 	return 0;
 }
